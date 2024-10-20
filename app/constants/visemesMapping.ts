c95b42e8d96251e230c7b48c8f882cb6062507fa
Vikranth Kumar <vikranthkumar@Vikranths-MacBook-Air.local>
2024-10-20 02:31:48 -0700

    finetuning lip syncying

--- app/constants/visemesMapping.ts
+++ app/constants/visemesMapping.ts
@@ -1,56 +1,47 @@
 const visemesMapping = {
-  A: "viseme_aa",
-  a: "viseme_aa",
-  B: "viseme_PP",
-  b: "viseme_PP",
-  C: "viseme_CH",
-  c: "viseme_CH",
-  D: "viseme_DD",
-  d: "viseme_DD",
-  E: "viseme_E",
-  e: "viseme_E",
-  F: "viseme_FF",
-  f: "viseme_FF",
-  G: "viseme_E",
-  g: "viseme_E",
-  H: "viseme_TH",
-  h: "viseme_TH",
-  I: "viseme_I",
-  i: "viseme_I",
-  J: "viseme_CH",
-  j: "viseme_CH",
-  K: "viseme_kk",
-  k: "viseme_kk",
-  L: "viseme_RR",
-  l: "viseme_RR",
-  M: "viseme_PP",
-  m: "viseme_PP",
-  N: "viseme_nn",
-  n: "viseme_nn",
-  O: "viseme_O",
-  o: "viseme_O",
-  P: "viseme_PP",
-  p: "viseme_PP",
-  Q: "viseme_U",
-  q: "viseme_U",
-  R: "viseme_RR",
-  r: "viseme_RR",
-  S: "viseme_SS",
-  s: "viseme_SS",
-  T: "viseme_TH",
-  t: "viseme_TH",
-  U: "viseme_U",
-  u: "viseme_U",
-  V: "viseme_DD",
-  v: "viseme_DD",
-  W: "viseme_U",
-  w: "viseme_U",
-  X: "viseme_SS",
-  x: "viseme_SS",
-  Y: "viseme_U",
-  y: "viseme_U",
-  Z: "viseme_SS",
-  z: "viseme_SS"
+  // Vowels
+  A: "viseme_aa", a: "viseme_aa",
+  E: "viseme_E",  e: "viseme_E",
+  I: "viseme_I",  i: "viseme_I",
+  O: "viseme_O",  o: "viseme_O",
+  U: "viseme_U",  u: "viseme_U",
+
+  // Bilabial (lips together)
+  B: "viseme_PP", b: "viseme_PP",
+  M: "viseme_PP", m: "viseme_PP",
+  P: "viseme_PP", p: "viseme_PP",
+
+  // Labiodental (teeth and lip)
+  F: "viseme_FF", f: "viseme_FF",
+  V: "viseme_DD", v: "viseme_DD",
+
+  // Alveolar (tongue to upper teeth or ridge)
+  D: "viseme_DD", d: "viseme_DD",
+  N: "viseme_nn", n: "viseme_nn",
+  T: "viseme_TH", t: "viseme_TH",
+  L: "viseme_RR", l: "viseme_RR",
+  R: "viseme_RR", r: "viseme_RR",
+
+  // Sibilant (hissing sounds)
+  S: "viseme_SS", s: "viseme_SS",
+  Z: "viseme_SS", z: "viseme_SS",
+  X: "viseme_SS", x: "viseme_SS",
+
+  // Velar (back of the tongue)
+  G: "viseme_E",  g: "viseme_E",
+  K: "viseme_kk", k: "viseme_kk",
+
+  // Palato-alveolar (tongue close to the roof of the mouth)
+  C: "viseme_CH", c: "viseme_CH",
+  J: "viseme_CH", j: "viseme_CH",
+
+  // Dental fricative
+  H: "viseme_TH", h: "viseme_TH",
+
+  // Special cases
+  Q: "viseme_U",  q: "viseme_U",
+  W: "viseme_U",  w: "viseme_U",
+  Y: "viseme_U",  y: "viseme_U"
 };
 
 export default visemesMapping;
